c6ae36fd36fd1e4765ad74bf8cab72b36173ac8f
Michael Okeowo <106069890+okeowo-mike@users.noreply.github.com>
2024-02-26 21:29:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// hamburger menu
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+// smooth scrolling ////////////////////////////
+
+$("#text-header a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
